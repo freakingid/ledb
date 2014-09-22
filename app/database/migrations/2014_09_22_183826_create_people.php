fa0a2ce310f4b18395ee4a64d86e9a8010951de9
Paul Kaiser <paul.kaiser@gmail.com>
2014-09-22 20:03:21 +0000

    Initial commit; Starting migrations;

---
+++ app/database/migrations/2014_09_22_183826_create_people.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePeople extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('people', function($table)
+		{
+		    $table->increments('id');
+		    $table->string('username', 128);
+		    $table->string('namelast', 64);
+		    $table->string('namefirst', 64);
+		    $table->date('dob');
+		    $table->string('email', 128);
+		    $table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('people');
+	}
+
+}
