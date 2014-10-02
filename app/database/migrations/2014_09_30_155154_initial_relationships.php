1bb3812e515009ad1496a01dbcf79897d5c78c1d
Paul Kaiser <paul.kaiser@gmail.com>
2014-10-02 21:23:49 +0000

    Finish initial migration rollback for initial relationships

--- app/database/migrations/2014_09_30_155154_initial_relationships.php
+++ app/database/migrations/2014_09_30_155154_initial_relationships.php
@@ -16,18 +16,18 @@ class InitialRelationships extends Migration {
 	    // create person to performance
 	    Schema::create('person_performance', function($table)
 	    {
-	        $table->integer('person_id')->unsigned();
+	        $table->integer('person_id')->unsigned()->default(0);
 	        $table->foreign('person_id')->references('id')->on('people');
-	        $table->integer('performance_id')->unsigned();
+	        $table->integer('performance_id')->unsigned()->default(0);
 	        $table->foreign('performance_id')->references('id')->on('performances');
 	    });
 	    
 	    // create person to artwork
 	    Schema::create('artwork_person', function($table)
 	    {
-	        $table->integer('artwork_id')->unsigned();
+	        $table->integer('artwork_id')->unsigned()->default(0);
 	        $table->foreign('artwork_id')->references('id')->on('artworks');
-	        $table->integer('person_id')->unsigned();
+	        $table->integer('person_id')->unsigned()->default(0);
 	        $table->foreign('person_id')->references('id')->on('people');
 	    });
 	    
@@ -37,28 +37,28 @@ class InitialRelationships extends Migration {
 	    // So, performance needs an artwork_id to reference the artwork it belongs to
 	    Schema::table('performances', function($table)
 	    {
-	        $table->integer('artwork_id')->unsigned();
+	        $table->integer('artwork_id')->unsigned()->default(0);
 	        $table->foreign('artwork_id')->references('id')->on('artworks');
 	    });
 	    
 	    // performance belongs_to event; event has_many performance;
 	    Schema::table('performances', function($table)
 	    {
-	        $table->integer('event_id')->unsigned();
+	        $table->integer('event_id')->unsigned()->default(0);
 	        $table->foreign('event_id')->references('id')->on('events');
 	    });
 	    
 	    // event belongs_to location; location has_many events;
 	    Schema::table('events', function($table)
 	    {
-	        $table->integer('location_id')->unsigned();
+	        $table->integer('location_id')->unsigned()->default(0);
 	        $table->foreign('location_id')->references('id')->on('locations');
 	    }); 
 	    
 	    // event belongs_to tour; tour has_many events;
 	    Schema::table('events', function($table)
 	    {
-	        $table->integer('tour_id')->unsigned();
+	        $table->integer('tour_id')->unsigned()->default(0);
 	        $table->foreign('tour_id')->references('id')->on('tours');
 	    });
 
@@ -81,13 +81,10 @@ class InitialRelationships extends Migration {
 		// remove one to many references
 		Schema::table('events', function($table)
 		{
-			$table->dropForeign('locations_id_foreign'); // TODO is this correct?
-			$table->dropForeign('tours_id_foreign'); // TODO is this correct?
 			$table->dropColumn(array('tour_id', 'location_id'));
 		});
 		Schema::table('performances', function($table)
 		{
-			$table->dropForeign('artworks_id_foreign'); // TODO is this correct?
 			$table->dropColumn(array('event_id', 'artwork_id'));
 		});
 
