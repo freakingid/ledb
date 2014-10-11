8a838eb21456e6892414c1a93cec8d0c8922614e
Paul Kaiser <paul.kaiser@gmail.com>
2014-10-11 15:24:20 +0000

    Fix deleting for Location

--- app/models/Location.php
+++ app/models/Location.php
@@ -7,6 +7,24 @@ class Location extends Eloquent
     // each location belongsto many events (surely you'll have more than one event at a venue in its lifetime)
     public function events()
     {
-        return $this->belongsToMany('EventOccurrence');
+        return $this->hasMany('EventOccurrence');
+    }
+
+    public function delete()
+    {
+        /*
+            1. Find events that have location_id == $this->id
+            2. Set location_id == null on those events
+            3. Save
+        */
+        $this->events()->get()->each( function( $event )
+        {
+            // unset event's reference to this location
+            $event->location_id = null;
+            $event->save();
+        });
+
+        // now delete the location
+        return parent::delete();        
     }
 }
