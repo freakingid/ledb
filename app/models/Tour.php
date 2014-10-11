98ec94ca2a83a1d99b636325b03c2cbab2b7bac1
Paul Kaiser <paul.kaiser@gmail.com>
2014-10-11 15:55:35 +0000

    Fix deleting for Person and Tour

--- app/models/Tour.php
+++ app/models/Tour.php
@@ -9,4 +9,22 @@ class Tour extends Eloquent
     {
         return $this->hasMany('EventOccurrence');
     }
+
+    public function delete()
+    {
+        /*
+            1. Find events that have tour_id == $this->id
+            2. Set tour_id == null on those events
+            3. Save
+        */
+        $this->events()->get()->each( function( $event )
+        {
+            // unset event's reference to this location
+            $event->tour_id = null;
+            $event->save();
+        });
+
+        // now delete the location
+        return parent::delete();        
+    }
 }
